6fb1bd52bb8e0278fa47b7a64eb9340173e4002d
satomio <mio.sato@broadleaf.co.jp>
2022-07-20 15:45:59 +0900

    登録を保存

--- application/src/main/java/com/example/application/UserController.java
+++ application/src/main/java/com/example/application/UserController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -33,11 +35,37 @@ public class UserController {
   }
 
   @RequestMapping("user/new")
-  public String regist(Model m) {
+  public String create(Model m) {
 
     List<UserEntity> list = userRepository.findAll();
     m.addAttribute("user", list);
 
     return "/new";
   }  
+
+  @GetMapping("/user/save")
+  public String save(Model m,
+  @RequestParam("name") String name,
+  @RequestParam("date") String date,
+  @RequestParam("time") String time,
+  @RequestParam("people") int people,
+  @RequestParam("tel") String tel
+  ) {
+    m.addAttribute("name", name);
+    m.addAttribute("date", date);
+    m.addAttribute("time", time);
+    m.addAttribute("people", people);
+    m.addAttribute("tel", tel);
+
+    UserEntity user = new UserEntity();
+    user.setName(name);
+    user.setDate(date);
+    user.setTime(time);
+    user.setPeople(people);
+    user.setTel(tel);
+
+    userRepository.save(user);
+
+    return "redirect:/user/show";
+  }
 }
