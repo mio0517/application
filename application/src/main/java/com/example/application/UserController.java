1f875c92301d37446193c5f63f48c975f9d8006f
satomio <mio.sato@broadleaf.co.jp>
2022-07-25 16:33:11 +0900

    内容追加

--- application/src/main/java/com/example/application/UserController.java
+++ application/src/main/java/com/example/application/UserController.java
@@ -5,8 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,11 +29,12 @@ public class UserController {
 
   }
 
-  @RequestMapping("user/detail")
-  public String detail(Model m) {
+  @RequestMapping("user/{id}/detail")
+  public String detail(Model m,
+  @PathVariable Integer id) {
 
-    List<UserEntity> list = userRepository.findAll();
-    m.addAttribute("user", list);
+    UserEntity list =userRepository.findById(id).get();
+    m.addAttribute("userInfo", list);
 
     return "user/detail";
   }
--- application/src/main/resources/schema.sql
+++ application/src/main/resources/schema.sql
@@ -12,4 +12,6 @@ CREATE TABLE `sample_db`.`list` (
   PRIMARY KEY (`id`));
 
 INSERT INTO `sample_db`.`list` (`id`, `name`, `genre`, `address`, `seats`, `opening`, `tel`, `countreserved`) VALUES ('1', '太郎', '中華', '東京都品川区1-1', '30', '9:00~21:00', '000-1111-2222', '0');
+INSERT INTO `sample_db`.`list` (`id`, `name`, `genre`, `address`, `seats`, `opening`, `tel`, `countreserved`) VALUES ('2', '太郎', '中華', '東京都品川区1-1', '30', '9:00~21:00', '000-1111-2222', '0');
+INSERT INTO `sample_db`.`list` (`id`, `name`, `genre`, `address`, `seats`, `opening`, `tel`, `countreserved`) VALUES ('3', '太郎', '中華', '東京都品川区1-1', '30', '9:00~21:00', '000-1111-2222', '0');
 
