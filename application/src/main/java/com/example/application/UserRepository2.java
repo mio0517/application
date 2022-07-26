846312ce6804fe7f149c44920845d0ffacbc5a10
satomio <mio.sato@broadleaf.co.jp>
2022-07-26 11:32:01 +0900

    リストボックス表示

--- application/src/main/java/com/example/application/UserRepository2.java
+++ application/src/main/java/com/example/application/UserRepository2.java
@@ -1,18 +1,18 @@
 package com.example.application;
 
-import java.util.List;
+//import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository2 extends JpaRepository<UserEntity2, Integer> {
   
-  @Query(
-    value="SELECT name1, store, date, time, people, tel1 FROM info WHERE store = user.store",
-    nativeQuery=true //直接SQLを実行する機能
-  )
-  public List<UserEntity2> findAllOrderByPriceDesc();
+  // @Query(
+  //   value="SELECT name1, store, date, time, people, tel1 FROM info WHERE store = user.store",
+  //   nativeQuery=true //直接SQLを実行する機能
+  // )
+  // public List<UserEntity2> findAllOrderByPriceDesc();
 
 }
