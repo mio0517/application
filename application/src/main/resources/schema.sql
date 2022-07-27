840a63f03b5f8c6abe67061eaae757f4c8993795
satomio <mio.sato@broadleaf.co.jp>
2022-07-28 08:58:02 +0900

    店舗追加

--- application/src/main/resources/schema.sql
+++ application/src/main/resources/schema.sql
@@ -15,4 +15,5 @@ INSERT INTO `sample_db`.`list` (`id`, `name`, `genre`, `address`, `seats`, `open
 INSERT INTO `sample_db`.`list` (`id`, `name`, `genre`, `address`, `seats`, `opening`, `tel`, `countreserved`) VALUES ('2', 'すき屋', '牛丼・丼もの', '東京都品川区1-1', '30', '9:00~21:00', '000-1111-2222', '0');
 INSERT INTO `sample_db`.`list` (`id`, `name`, `genre`, `address`, `seats`, `opening`, `tel`, `countreserved`) VALUES ('3', '丸亀製麺', 'うどん', '東京都品川区1-1', '30', '9:00~21:00', '000-1111-2222', '0');
 INSERT INTO `sample_db`.`list` (`id`, `name`, `genre`, `address`, `seats`, `opening`, `tel`, `countreserved`) VALUES ('4', 'びっくりドンキー', 'ハンバーグ・ステーキ', '東京都品川区1-1', '30', '9:00~21:00', '000-1111-2222', '0');
+INSERT INTO `sample_db`.`list` (`id`, `name`, `genre`, `address`, `seats`, `opening`, `tel`, `countreserved`) VALUES ('5', 'ココス', 'ハンバーグ・ステーキ', '東京都品川区1-1', '30', '9:00~21:00', '000-1111-2222', '0');
 
