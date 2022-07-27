21e1c993a4bb4cf016bcd28eee1f7c27101c4eb3
Naoto Hori <naoto.hori@broadleaf.co.jp>
2022-07-27 18:27:12 +0900

    検索機能の追加

--- application/src/main/java/com/example/application/UserRepository.java
+++ application/src/main/java/com/example/application/UserRepository.java
@@ -1,12 +1,21 @@
 package com.example.application;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-  
 
- 
+    @Query(value = "SELECT * FROM sample_db.list where "
+            + "sample_db.list.name like CONCAT('%', :searchKey, '%')"
+            + "or sample_db.list.genre like CONCAT('%', :searchKey, '%')"
+            + "or sample_db.list.address like CONCAT('%', :searchKey, '%')"
+            + "or sample_db.list.opening like CONCAT('%', :searchKey, '%')"
+            + "or sample_db.list.tel like CONCAT('%', :searchKey, '%')", nativeQuery = true)
+    public List<UserEntity> findAllLike(@Param("searchKey") String searchKey);
 
 }
-
