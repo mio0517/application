4c574627c89ed473d343fc976705bbf509a0e543
satomio <mio.sato@broadleaf.co.jp>
2022-07-28 11:03:00 +0900

    表示内容変更

--- application/src/main/java/com/example/application/UserController.java
+++ application/src/main/java/com/example/application/UserController.java
@@ -1,11 +1,8 @@
 package com.example.application;
 
-import java.util.ArrayList;
 import java.util.List;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- application/src/main/resources/data.sql
+++ application/src/main/resources/data.sql
@@ -11,5 +11,6 @@ CREATE TABLE `sample_db`.`info` (
   `favorite` INTEGER,
   PRIMARY KEY (`id`));
 
-  INSERT INTO `sample_db`.`info` (`id`, `name1`, `store`, `date`, `time`, `people`, `tel1`, `favorite`) VALUES ('1', 'あああ',  '1', '7/19', '18:00~', '5', '111-222-3333', '1');
-  INSERT INTO `sample_db`.`info` (`id`, `name1`, `store`, `date`, `time`, `people`, `tel1`, `favorite`) VALUES ('2', 'いいいい',  '2', '7/19', '18:00~', '5', '111-222-3333', '1');
+  INSERT INTO `sample_db`.`info` (`id`, `name1`, `store`, `date`, `time`, `people`, `tel1`, `favorite`) VALUES ('1', '堀直人',  '1', '7/19', '18:00~', '2', '111-222-3333', '1');
+  INSERT INTO `sample_db`.`info` (`id`, `name1`, `store`, `date`, `time`, `people`, `tel1`, `favorite`) VALUES ('2', '後藤興国',  '2', '7/28', '20:00~', '3', '111-222-3333', '1');
+  INSERT INTO `sample_db`.`info` (`id`, `name1`, `store`, `date`, `time`, `people`, `tel1`, `favorite`) VALUES ('2', '福西恵美',  '3', '7/20', '19:00~', '2', '111-222-3333', '1');
